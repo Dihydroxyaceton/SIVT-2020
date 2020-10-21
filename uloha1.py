ceff868ff138b4e8be3f6d669254d3756edb9f5c
Dihydroxyaceton <73236021+Dihydroxyaceton@users.noreply.github.com>
2020-10-21 16:03:03 +0200

    test file upload

---
+++ uloha1.py
@@ -0,0 +1,8 @@
+#Vstup uživatele: input
+
+slovo=input("Napiš něco:")
+print("Tvoje slovo je "+slovo)
+
+cislo=int(input("Napiš číslo: "))
+desetinasobek=cislo*10
+print("Desetinásobek je: "+str(desetinasobek))
