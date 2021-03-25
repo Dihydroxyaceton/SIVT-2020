e76b80e49ebb0024041c9cf08e3a120925d31a6d
Dihydroxyaceton <73236021+Dihydroxyaceton@users.noreply.github.com>
2021-03-25 12:37:22 +0100

    Update zlomky.py

--- aproximace zlomku/zlomky.py
+++ aproximace zlomku/zlomky.py
@@ -2,8 +2,8 @@ import math
 
 inputnumber = float(input("enter your number: "))
 
-tolerance = 0.001
-maxnumber = 500
+tolerance = 0.0001
+maxnumber = 1000
 
 # čitatel = numerator
 # jmenovatel = nominator
@@ -11,26 +11,21 @@ maxnumber = 500
 print("Please stand by...")
 
 ok_results = []
-numerator = 1
+numerator = 1 
 nominator = 1
-
 
 def check_tolerance():
 	#print (inputnumber-outcome)
 	if (inputnumber-outcome) < tolerance:
 		if (inputnumber-outcome) > -tolerance:
-			#print ("ok")
 			ok_results.append(str(numerator)+"/"+str(nominator))
-
 
 for i in range(maxnumber):
 	for i in range(maxnumber):
 		outcome = numerator / nominator
 		check_tolerance()
-		#print(numerator)
 		numerator += 1
 	numerator = 1
 	nominator += 1
 
-#print(inputnumber)
 print(ok_results)
